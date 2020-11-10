71eb36bc6a82dc0cd3e669270e227c7f753a6be4
ayush raj <ayushrajpoot97@gmail.com>
2020-11-10 20:51:26 +0530

    added shopping cart mini project

---
+++ shoppingcart/src/Components/BuyPage.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import CartItem from "./CartItem";
+
+import { random, commerce } from "faker";
+import { Container, Col, Row } from "reactstrap";
+
+const apiKey = "563492ad6f917000010000012097ec4f33834ff1bdbf87303b61c7ae";
+
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+const localurl = "https://api.myjson.com/bins/qp9uo";
+
+
+
+
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
+
+    const fetchPhotos = async () => {
+      const { data }  = await Axios.get(url, {
+        headers: {
+                 Authorization: apiKey
+                }
+      });
+      
+      const { photos } = data;
+
+    const allProduct = photos.map(photo => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid()
+    }));
+
+    setProduct(allProduct);
+      
+      
+      
+  };
+
+  // const fetchPhotos = async () => {
+  //   const { data } = await Axios.get(localurl, {});
+
+  //   const { photos } = data;
+
+  //   const allProduct = photos.map(photo => ({
+  //     smallImage: photo.src.medium,
+  //     tinyImage: photo.src.tiny,
+  //     productName: random.word(),
+  //     productPrice: commerce.price(),
+  //     id: random.uuid()
+  //   }));
+
+  //   setProduct(allProduct);
+  // };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map(product => (
+          <Col md={4} key={product.id}>
+            <CartItem product={product} addInCart={addInCart} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default BuyPage;
